43781d975a664fcbe61b21153407740cc68464d5
Ulrich Hecht <ulrich.hecht@gmail.com>
2014-11-21 11:58:04 +0100

    acd: AC.h: convert #pragma fastcall to C prototypes
    Signed-off-by: Ulrich Hecht <ulrich.hecht@gmail.com>

--- include/pce/AC.h
+++ include/pce/AC.h
@@ -7,8 +7,6 @@
  */
 
 
-
-
 /*
  *	Unused test. single 24bit argument passing.
  */
@@ -19,10 +17,10 @@
  *	Arcade card address reg function: 24bit value, 1 byte(high) and 1 word(mid/low). 
  */ 
 
-#pragma fastcall ac_addr_reg0( byte ac_reg_1_high, word ac_reg_1_low ) nop;
-#pragma fastcall ac_addr_reg1( byte ac_reg_2_high, word ac_reg_2_low ) nop;
-#pragma fastcall ac_addr_reg2( byte ac_reg_3_high, word ac_reg_3_low ) nop;
-#pragma fastcall ac_addr_reg3( byte ac_reg_4_high, word ac_reg_4_low ) nop;
+void __fastcall __nop ac_addr_reg0( unsigned char high<ac_reg_1_high>, unsigned int low<ac_reg_1_low> );
+void __fastcall __nop ac_addr_reg1( unsigned char high<ac_reg_2_high>, unsigned int low<ac_reg_2_low> );
+void __fastcall __nop ac_addr_reg2( unsigned char high<ac_reg_3_high>, unsigned int low<ac_reg_3_low> );
+void __fastcall __nop ac_addr_reg3( unsigned char high<ac_reg_4_high>, unsigned int low<ac_reg_4_low> );
 
 
 /*
@@ -30,78 +28,56 @@
  *	word increment, byte control. *Inlined*
  *
  */
-#pragma fastcall ac_full_reg0( byte ac_reg_1_high, word ac_reg_1_low, word ac_reg_1_offset_l, word ac_reg_1_incrmt_l, byte ac_reg_1_control) nop;
-#pragma fastcall ac_full_reg1( byte ac_reg_2_high, word ac_reg_2_low, word ac_reg_2_offset_l, word ac_reg_2_incrmt_l, byte ac_reg_2_control) nop;
-#pragma fastcall ac_full_reg2( byte ac_reg_3_high, word ac_reg_3_low, word ac_reg_3_offset_l, word ac_reg_3_incrmt_l, byte ac_reg_3_control) nop;
-#pragma fastcall ac_full_reg3( byte ac_reg_4_high, word ac_reg_4_low, word ac_reg_4_offset_l, word ac_reg_4_incrmt_l, byte ac_reg_4_control) nop;
+void __fastcall __nop ac_full_reg0( unsigned char high<ac_reg_1_high>, unsigned int low<ac_reg_1_low>, unsigned int offset_l<ac_reg_1_offset_l>, unsigned int incrmt_l<ac_reg_1_incrmt_l>, unsigned char control<ac_reg_1_control>);
+void __fastcall __nop ac_full_reg1( unsigned char high<ac_reg_2_high>, unsigned int low<ac_reg_2_low>, unsigned int offset_l<ac_reg_2_offset_l>, unsigned int incrmt_l<ac_reg_2_incrmt_l>, unsigned char control<ac_reg_2_control>);
+void __fastcall __nop ac_full_reg2( unsigned char high<ac_reg_3_high>, unsigned int low<ac_reg_3_low>, unsigned int offset_l<ac_reg_3_offset_l>, unsigned int incrmt_l<ac_reg_3_incrmt_l>, unsigned char control<ac_reg_3_control>);
+void __fastcall __nop ac_full_reg3( unsigned char high<ac_reg_4_high>, unsigned int low<ac_reg_4_low>, unsigned int offset_l<ac_reg_4_offset_l>, unsigned int incrmt_l<ac_reg_4_incrmt_l>, unsigned char control<ac_reg_4_control>);
 
 
 /*
  *	Arcade card increment reg function: word size. *Inlined*
  */
-#pragma fastcall ac_inc_reg0( word ac_reg_1_incrmt_l ) nop;
-#pragma fastcall ac_inc_reg1( word ac_reg_2_incrmt_l ) nop;
-#pragma fastcall ac_inc_reg2( word ac_reg_3_incrmt_l ) nop;
-#pragma fastcall ac_inc_reg3( word ac_reg_4_incrmt_l ) nop;
+void __fastcall __nop ac_inc_reg0( unsigned int incrmt_l<ac_reg_1_incrmt_l> );
+void __fastcall __nop ac_inc_reg1( unsigned int incrmt_l<ac_reg_2_incrmt_l> );
+void __fastcall __nop ac_inc_reg2( unsigned int incrmt_l<ac_reg_3_incrmt_l> );
+void __fastcall __nop ac_inc_reg3( unsigned int incrmt_l<ac_reg_4_incrmt_l> );
 
 
 /*
  *	Arcade card offset reg function: word size. *Inlined*
  */
-#pragma fastcall ac_offset_reg0( word ac_reg_1_offset_l ) nop;
-#pragma fastcall ac_offset_reg1( word ac_reg_2_offset_l ) nop;
-#pragma fastcall ac_offset_reg2( word ac_reg_3_offset_l ) nop;
-#pragma fastcall ac_offset_reg3( word ac_reg_4_offset_l ) nop;
+void __fastcall __nop ac_offset_reg0( unsigned int offset_l<ac_reg_1_offset_l> );
+void __fastcall __nop ac_offset_reg1( unsigned int offset_l<ac_reg_2_offset_l> );
+void __fastcall __nop ac_offset_reg2( unsigned int offset_l<ac_reg_3_offset_l> );
+void __fastcall __nop ac_offset_reg3( unsigned int offset_l<ac_reg_4_offset_l> );
 
 
 /*
  *	Arcade card control reg function: byte size. *Inlined*
  */
-#pragma fastcall ac_control_reg0( byte ac_reg_1_control_l ) nop;
-#pragma fastcall ac_control_reg1( byte ac_reg_2_control_l ) nop;
-#pragma fastcall ac_control_reg2( byte ac_reg_3_control_l ) nop;
-#pragma fastcall ac_control_reg3( byte ac_reg_4_control_l ) nop;
+void __fastcall __nop ac_control_reg0( unsigned char control<ac_reg_1_control_l> );
+void __fastcall __nop ac_control_reg1( unsigned char control<ac_reg_2_control_l> );
+void __fastcall __nop ac_control_reg2( unsigned char control<ac_reg_3_control_l> );
+void __fastcall __nop ac_control_reg3( unsigned char control<ac_reg_4_control_l> );
 
 
 /*
  *	CD sector to Arcade card transfer.
- *	
- *	ac_cd_xfer( ac reg (byte), sector addr high(byte),sector addr low(word),
- *	            sectors (byte) )
  */
 void __fastcall ac_cd_xfer( unsigned char ac_reg<bl>, unsigned char sector_addr_h<cl>, unsigned int sector_addr_l<dx>, unsigned char sectors<al> );
 
+void __fastcall ac_vram_xfer(unsigned char ac_reg<al>, unsigned int vram_addr<bx>, unsigned int num_bytes<cx>, unsigned char size<dl> );
+void __fastcall ac_vram_xfer(unsigned char ac_reg<al>, unsigned int vram_addr<bx>, unsigned int num_bytes<cx>, unsigned char size<dl>, unsigned char flags<ah> );
 
-/*
- * ac_vram_xfer( AC reg (word), vram addr (word), num bytes (word), chunk size(byte))
- * ac_vram_xfer( AC reg (word), vram addr (word), num bytes (word), chunk size(byte), const SGX )
- */
-#pragma fastcall ac_vram_xfer(byte al, word bx, word cx, byte dl );
-#pragma fastcall ac_vram_xfer(byte al, word bx, word cx, byte dl, byte ah );
+void __fastcall ac_vram_dma( unsigned char ac_reg<al>, unsigned int vram_addr<bx>, unsigned int num_bytes<cx> );
+void __fastcall ac_vram_dma( unsigned char ac_reg<al>, unsigned int vram_addr<bx>, unsigned int num_bytes<cx>, unsigned char flags<ah> );
 
 
+void __fastcall ac_vram_copy( unsigned char ac_reg<al>, unsigned int vram_addr<bx>, unsigned int bytes<cx> );
+void __fastcall ac_vram_copy( unsigned char ac_reg<al>, unsigned int vram_addr<bx>, unsigned int bytes<cx>, unsigned char flags<ah> );
 
 
-/*
- * ac_vram_dma( AC reg (word), vram addr (word), num bytes (word) )
- * ac_vram_dma( AC reg (word), vram addr (word), num bytes (word), const SGX )
- */
-#pragma fastcall ac_vram_dma( byte al, word bx, word cx );
-#pragma fastcall ac_vram_dma( byte al, word bx, word cx, byte ah );
-
-
-/*
- * ac_vram_copy( AC reg (word), vram addr (word), num bytes (word) )
- * ac_vram_copy( AC reg (word), vram addr (word), num bytes (word), const SGX )
- */
-#pragma fastcall ac_vram_copy( byte al, word bx, word cx );
-#pragma fastcall ac_vram_copy( byte al, word bx, word cx, byte ah );
-
-
-/*
- * ac_vram_copy( AC reg (word), color number (word), num colors (word) )
- */
-#pragma fastcall ac_vce_copy( byte al, word bx, word cx );
+void __fastcall ac_vce_copy( unsigned char ac_reg<al>, unsigned int start_color<bx>, unsigned int num_colors<cx> );
 
 
 /*
