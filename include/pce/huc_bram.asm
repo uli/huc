3b3901f9eb8963cadc5c8834d4542aa18a38fc00
Ulrich Hecht <ulrich.hecht@gmail.com>
2014-11-22 12:05:23 +0100

    huc: huc_bram.asm: sanitize formatting
    Whitespace and spelling; converted from CRLF to LF. No functional changes.
    (This was uncharacteristically well-formatted to begin with...)
    
    Signed-off-by: Ulrich Hecht <ulrich.hecht@gmail.com>

--- include/pce/huc_bram.asm
+++ include/pce/huc_bram.asm
@@ -2,10 +2,10 @@
 ; HUC_BRAM.ASM  -  HuC Backup RAM Library
 ;
 
-		.bss
+	.bss
 _bm_mpr4:	.ds 1
 _bm_error:	.ds 1
-		.code
+	.code
 
 ; NOTE: the BRAM format is as follows:
 ;
@@ -56,26 +56,26 @@ _bm_error:	.ds 1
 ;
 
 _bm_check:
-	  jsr	_bm_unlock
+	jsr	_bm_unlock
 
-	  stw	#$8000,<_di	; test area at $8000
-	  jsr	_bm_testram
+	stw	#$8000,<_di	; test area at $8000
+	jsr	_bm_testram
 
-	  ; -- result
-	  jsr	_bm_disable
-	  cpx	#0
-	  bne	.err
-	  ; -- ok
-	  stz	_bm_error
-	  ldx	#1
-	  cla
-	  clc
-	  rts
-	  ; -- no bram
-.err:	  clx
-	  cla
-	  sec
-	  rts
+	; -- result
+	jsr	_bm_disable
+	cpx	#0
+	bne	.err
+	; -- ok
+	stz	_bm_error
+	ldx	#1
+	cla
+	clc
+	rts
+	; -- no bram
+.err:	clx
+	cla
+	sec
+	rts
 
 
 ; bm_testram
@@ -86,27 +86,27 @@ _bm_check:
 ;
 
 _bm_testram:
-	  ; -- swap bits
-	  ldy	#7
-.l1:	  lda	[_di],Y
-	  eor	#$FF
-	  sta	_ax,Y
-	  sta	[_di],Y
-	  dey
-	  bpl	.l1
-	  ; -- cmp
-	  clx
-	  ldy	#7
-.l2:	  lda	_ax,Y
-	  cmp	[_di],Y
-	  beq	.l3
-	  inx
-.l3:	  eor	#$FF
-	  sta	[_di],Y
-	  dey
-	  bpl	.l2
+	; -- swap bits
+	ldy	#7
+.l1:	lda	[_di],Y
+	eor	#$FF
+	sta	_ax,Y
+	sta	[_di],Y
+	dey
+	bpl	.l1
+	; -- cmp
+	clx
+	ldy	#7
+.l2:	lda	_ax,Y
+	cmp	[_di],Y
+	beq	.l3
+	inx
+.l3:	eor	#$FF
+	sta	[_di],Y
+	dey
+	bpl	.l2
 
-	  rts
+	rts
 
 
 ; bm_format()
@@ -117,38 +117,38 @@ _bm_testram:
 ;
 
 _bm_format:
-	  jsr	_bm_enable
-	  bcc	.ok
-	  ; -- format
-	  ldx	#7
-.l1:	  lda	_bm_id,X
-	  sta	$8000,X
-	  dex
-	  bpl	.l1
-	  stz	$8010
-	  stz	$8011
+	jsr	_bm_enable
+	bcc	.ok
+	; -- format
+	ldx	#7
+.l1:	lda	_bm_id,X
+	sta	$8000,X
+	dex
+	bpl	.l1
+	stz	$8010
+	stz	$8011
 
-	  stw	#$8000,<_di	; test area at $8000
-.l2:	  jsr	_bm_testram
-	  cpx	#0
-	  bne	.setsz
-	  lda	<_di+1
-	  cmp	#$A0		; and keep going until either
-	  beq	.setsz		; (a) bad memory, or
-	  inc	A		; (b) next bank
-	  sta	<_di+1
-	  bra	.l2
+	stw	#$8000,<_di	; test area at $8000
+.l2:	jsr	_bm_testram
+	cpx	#0
+	bne	.setsz
+	lda	<_di+1
+	cmp	#$A0		; and keep going until either
+	beq	.setsz		; (a) bad memory, or
+	inc	A		; (b) next bank
+	sta	<_di+1
+	bra	.l2
 
-.setsz:	  lda	<_di+1
-	  sta	$8005
+.setsz:	lda	<_di+1
+	sta	$8005
 
-	  ; -- ok
-.ok:	  jsr	_bm_disable
-	  stz	_bm_error
-	  clx
-	  cla
-	  clc
-	  rts
+	; -- ok
+.ok:	jsr	_bm_disable
+	stz	_bm_error
+	clx
+	cla
+	clc
+	rts
 
 
 ; bm_free()
@@ -158,28 +158,28 @@ _bm_format:
 ;
 
 _bm_free:
-	  jsr	_bm_enable
-	  bcs	.err
-	  ; -- calculate free space
-	  stw	$8004,<_cx
-	  subw	$8006,<_cx
-	  subw	#$12,<_cx
-	  lda	<_ch
-	  bpl	.ok
-	  stwz	<_cx
-	  ; -- ok
-.ok:	  jsr	_bm_disable
-	  stz	_bm_error
+	jsr	_bm_enable
+	bcs	.err
+	; -- calculate free space
+	stw	$8004,<_cx
+	subw	$8006,<_cx
+	subw	#$12,<_cx
+	lda	<_ch
+	bpl	.ok
+	stwz	<_cx
+	; -- ok
+.ok:	jsr	_bm_disable
+	stz	_bm_error
 	__ldw	<_cx
-	  clc
-	  rts
-	  ; -- error, bram not formated
-.err:	  sta	_bm_error
-	  jsr	_bm_disable
-	  lda	#$FF
-	  tax
-	  sec
-	  rts
+	clc
+	rts
+	; -- error, bram not formatted
+.err:	sta	_bm_error
+	jsr	_bm_disable
+	lda	#$FF
+	tax
+	sec
+	rts
 
 
 ; bm_size()
@@ -188,31 +188,31 @@ _bm_free:
 ; be 2K, but can be as large as 8KB
 
 _bm_size:
-	  jsr	_bm_enable
-	  bcs	.err
-	  ; -- calculate free space
-	  stw	$8004,<_cx
-	  subw	#$8000,<_cx
-	  lda	<_ch
-	  cmp	#$21
-	  bcs	.err1
-	  lda	<_ch
-	  bpl	.ok
-	  stwz	<_cx
-	  ; -- ok
-.ok:	  jsr	_bm_disable
-	  stz	_bm_error
+	jsr	_bm_enable
+	bcs	.err
+	; -- calculate free space
+	stw	$8004,<_cx
+	subw	#$8000,<_cx
+	lda	<_ch
+	cmp	#$21
+	bcs	.err1
+	lda	<_ch
+	bpl	.ok
+	stwz	<_cx
+	; -- ok
+.ok:	jsr	_bm_disable
+	stz	_bm_error
 	__ldw	<_cx
-	  clc
-	  rts
-	  ; -- error, bram not formated
-.err1:	  lda	#$ff
-.err:	  sta	_bm_error
-	  jsr	_bm_disable
-	  lda	#$FF
-	  tax
-	  sec
-	  rts
+	clc
+	rts
+	; -- error, bram not formatted
+.err1:	lda	#$ff
+.err:	sta	_bm_error
+	jsr	_bm_disable
+	lda	#$FF
+	tax
+	sec
+	rts
 
 
 ; bm_rawread(int location)
@@ -222,23 +222,23 @@ _bm_size:
 ;
 _bm_rawread:
 	__stw	<_bx
-	  lda	<_bh
-	  and	#$1F
-	  ora	#$80
-	  sta	<_bh
-	  jsr	_bm_enable
-	  bcs	.err
-	  lda	[_bx]
-	  sax
-	  stz	_bm_error
-	  jsr	_bm_disable
-	  cla
-	  clc
-	  rts
-.err:	  sta	_bm_error
-	  jsr	_bm_disable
-	  sec
-	  rts
+	lda	<_bh
+	and	#$1F
+	ora	#$80
+	sta	<_bh
+	jsr	_bm_enable
+	bcs	.err
+	lda	[_bx]
+	sax
+	stz	_bm_error
+	jsr	_bm_disable
+	cla
+	clc
+	rts
+.err:	sta	_bm_error
+	jsr	_bm_disable
+	sec
+	rts
 
 
 ; bm_rawwrite(int location [bx], char val [reg acc])
@@ -248,24 +248,24 @@ _bm_rawread:
 ;
 _bm_rawwrite.2:
 	__stw	<_ax
-	  lda	<_bh
-	  and	#$1F
-	  ora	#$80
-	  sta	<_bh
-	  jsr	_bm_enable
-	  bcs	.err
-	  lda	<_al
-	  sta	[_bx]
-	  stz	_bm_error
-	  jsr	_bm_disable
-	  cla
-	  clx
-	  clc
-	  rts
-.err:	  sta	_bm_error
-	  jsr	_bm_disable
-	  sec
-	  rts
+	lda	<_bh
+	and	#$1F
+	ora	#$80
+	sta	<_bh
+	jsr	_bm_enable
+	bcs	.err
+	lda	<_al
+	sta	[_bx]
+	stz	_bm_error
+	jsr	_bm_disable
+	cla
+	clx
+	clc
+	rts
+.err:	sta	_bm_error
+	jsr	_bm_disable
+	sec
+	rts
 
 
 ; bm_exist(char *name)
@@ -278,15 +278,15 @@ _bm_rawwrite.2:
 
 _bm_exist:
 	__stw	<_bx
-	  jsr	_bm_open
-	  bcs	.l1
-	  jsr	_bm_disable
-	  ldx	#1
-	  lda	_bm_error
-	  beq	.noerr
-.l1:	  clx
-	  cla
-.noerr:	  rts
+	jsr	_bm_open
+	bcs	.l1
+	jsr	_bm_disable
+	ldx	#1
+	lda	_bm_error
+	beq	.noerr
+.l1:	clx
+	cla
+.noerr:	rts
 
 
 ; bm_sizeof(char *name)
@@ -299,17 +299,17 @@ _bm_exist:
 
 _bm_sizeof:
 	__stw	<_bx
-	  jsr	_bm_open
-	  bcs	.l1
-	  subw	#$10,<_cx
-	  jsr	_bm_disable
-	  stz	_bm_error
-	  ldx	<_cl
-	  lda	<_ch
-	  rts
-.l1:	  clx
-	  cla
-	  rts
+	jsr	_bm_open
+	bcs	.l1
+	subw	#$10,<_cx
+	jsr	_bm_disable
+	stz	_bm_error
+	ldx	<_cl
+	lda	<_ch
+	rts
+.l1:	clx
+	cla
+	rts
 
 
 ; bm_errno()
@@ -318,14 +318,14 @@ _bm_sizeof:
 ;
 
 _bm_errno:
-	  ldx	_bm_error
-	  cla
-	  rts
+	ldx	_bm_error
+	cla
+	rts
 
 
 _bm_id:
-	  .db	$48,$55,$42,$4D
-	  .db	$00,$88,$10,$80
+	.db	$48,$55,$42,$4D
+	.db	$00,$88,$10,$80
 
 
 ; ------------------------
@@ -338,8 +338,8 @@ _bm_id:
 ;
 
 _bm_getptr.2:
-	  maplibfunc	lib2_bm_getptr.2
-	  rts
+	maplibfunc	lib2_bm_getptr.2
+	rts
 
 
 ; bm_delete(char *namebuf)
@@ -348,8 +348,8 @@ _bm_getptr.2:
 ;
 
 _bm_delete:
-	  maplibfunc	lib2_bm_delete
-	  rts
+	maplibfunc	lib2_bm_delete
+	rts
 
 
 ; bm_read(char *buf [di], char *name [bx], int offset [bp], int nb)
@@ -358,8 +358,8 @@ _bm_delete:
 ;
 
 _bm_read.4:
-	  maplibfunc	lib2_bm_read.4
-	  rts
+	maplibfunc	lib2_bm_read.4
+	rts
 
 
 ; bm_write(char *buf [di], char *name [bx], int offset [bp], int nb)
@@ -368,8 +368,8 @@ _bm_read.4:
 ;
 
 _bm_write.4:
-	  maplibfunc	lib2_bm_write.4
-	  rts
+	maplibfunc	lib2_bm_write.4
+	rts
 
 
 ; bm_create(char *name [bx], int size)
@@ -377,8 +377,8 @@ _bm_write.4:
 ; Create a new BRAM file, given the name and size
 
 _bm_create.2:
-	  maplibfunc	lib2_bm_create.2
-	  rts
+	maplibfunc	lib2_bm_create.2
+	rts
 
 
 ; bm_open(char *name [bx])
@@ -386,8 +386,8 @@ _bm_create.2:
 ; Internal function to obtain access to a named file
 
 _bm_open:
-	  maplibfunc	lib2_bm_open
-	  rts
+	maplibfunc	lib2_bm_open
+	rts
 
 
 ; bm_enable()
@@ -395,8 +395,8 @@ _bm_open:
 ; Internal function to enable the BRAM area and do a quick check
 
 _bm_enable:
-	  maplibfunc	lib2_bm_enable
-	  rts
+	maplibfunc	lib2_bm_enable
+	rts
 
 
 ; bm_disable()
@@ -404,8 +404,8 @@ _bm_enable:
 ; This internal function handles the fixup of BRAM segment/locking
 
 _bm_disable:
-	  maplibfunc	lib2_bm_disable
-	  rts
+	maplibfunc	lib2_bm_disable
+	rts
 
 
 ; bm_unlock()
@@ -413,8 +413,8 @@ _bm_disable:
 ; This internal function handles only the map/unlock of the BRAM area
 
 _bm_unlock:
-	  maplibfunc	lib2_bm_unlock
-	  rts
+	maplibfunc	lib2_bm_unlock
+	rts
 
 
 ; ---------------
@@ -440,40 +440,40 @@ _bm_unlock:
 
 lib2_bm_getptr.2:
 	__stw	<_di		; namebuf is destination of a copy
-	  jsr	lib2_bm_enable
-	  bcs	.x2
+	jsr	lib2_bm_enable
+	bcs	.x2
 
-	  tstw	<_bp		; error - 0 input
-	  beq	.x2
-	  lda	[_bp]
-	  sta	<_cl
-	  ldy	#1
-	  lda	[_bp],Y
-	  sta	<_ch		; <_cx is length of entry
-	  tstw	<_cx
-	  beq	.empty
+	tstw	<_bp		; error - 0 input
+	beq	.x2
+	lda	[_bp]
+	sta	<_cl
+	ldy	#1
+	lda	[_bp],Y
+	sta	<_ch		; <_cx is length of entry
+	tstw	<_cx
+	beq	.empty
 
-	  addw	#4,<_bp,<_si	; <_si is now ptr to name of current entry
-	  cla
-	  ldx	#12
-	  jsr	_memcpy.3	; copy 12 bytes of name to namebuf
-	  addw	<_cx,<_bp,<_ax	; next pointer
-	  jsr	lib2_bm_disable
-	  stz	_bm_error
-	  lda	<_ah
-	  ldx	<_al
-	  clc
-	  rts
+	addw	#4,<_bp,<_si	; <_si is now ptr to name of current entry
+	cla
+	ldx	#12
+	jsr	_memcpy.3	; copy 12 bytes of name to namebuf
+	addw	<_cx,<_bp,<_ax	; next pointer
+	jsr	lib2_bm_disable
+	stz	_bm_error
+	lda	<_ah
+	ldx	<_al
+	clc
+	rts
 
-.empty:	  cla
-	  
-	  ; -- error, bram not formatted
-.x2:	  sta	_bm_error
-	  jsr	lib2_bm_disable
-	  cla
-	  clx
-	  sec
-	  rts
+.empty:	cla
+	
+	; -- error, bram not formatted
+.x2:	sta	_bm_error
+	jsr	lib2_bm_disable
+	cla
+	clx
+	sec
+	rts
 
 
 ; bm_delete(char *namebuf)
@@ -483,23 +483,23 @@ lib2_bm_getptr.2:
 
 lib2_bm_delete:
 	__stw	<_ax
-	  jsr	lib2_bm_open
-	  bcs	.out
-	  stw	$8006,<_bx	; ptr to end
-	  stw	<_si,<_di	; setup currptr as dest
-	  stw	<_dx,<_si	; setup nextptr as src
-	  subw	<_dx,<_bx	; #bytes = end-next + 2
-	  addw	#2,<_bx
-	  subw	<_cx,$8006	; adjust ptr to end
-	  lda	<_bh
-	  ldx	<_bl
-	  jsr	_memcpy.3
-	  jsr	lib2_bm_disable
-	  stz	_bm_error
-	  clx
-	  cla
-	  clc
-.out:	  rts
+	jsr	lib2_bm_open
+	bcs	.out
+	stw	$8006,<_bx	; ptr to end
+	stw	<_si,<_di	; setup currptr as dest
+	stw	<_dx,<_si	; setup nextptr as src
+	subw	<_dx,<_bx	; #bytes = end-next + 2
+	addw	#2,<_bx
+	subw	<_cx,$8006	; adjust ptr to end
+	lda	<_bh
+	ldx	<_bl
+	jsr	_memcpy.3
+	jsr	lib2_bm_disable
+	stz	_bm_error
+	clx
+	cla
+	clc
+.out:	rts
 
 
 ; bm_read(char *buf [di], char *name [bx], int offset [bp], int nb)
@@ -509,49 +509,49 @@ lib2_bm_delete:
 
 lib2_bm_read.4:
 	__stw	<_ax
-	  ; -- open file
-	  jsr	lib2_bm_open
-	  bcs	.x2
-	  ; -- checksum test
-	  jsr	lib2_bm_checksum
-	  ldy	#2
-	  lda	[_si],Y
-	  add	<_dl
-	  sta	<_dl
-	  iny
-	  lda	[_si],Y
-	  adc	<_dh
-	  ora	<_dl
-	  bne	.x1
-	  ; -- setup ptr
-	  jsr	lib2_bm_setup_ptr
-	  bcs	.ok
-	  ; -- read
-	  cly
-.l1:	  lda	[_dx],Y
-	  sta	[_di],Y
-	  iny
-	  bne	.l2
-	  inc	<_dx+1
-	  inc	<_di+1
-.l2:	  dec	<_cl
-	  bne	.l1
-	  dec	<_ch
-	  bpl	.l1
-	  ; -- ok
-.ok:	  jsr	lib2_bm_disable
-	  stz	_bm_error
+	; -- open file
+	jsr	lib2_bm_open
+	bcs	.x2
+	; -- checksum test
+	jsr	lib2_bm_checksum
+	ldy	#2
+	lda	[_si],Y
+	add	<_dl
+	sta	<_dl
+	iny
+	lda	[_si],Y
+	adc	<_dh
+	ora	<_dl
+	bne	.x1
+	; -- setup ptr
+	jsr	lib2_bm_setup_ptr
+	bcs	.ok
+	; -- read
+	cly
+.l1:	lda	[_dx],Y
+	sta	[_di],Y
+	iny
+	bne	.l2
+	inc	<_dx+1
+	inc	<_di+1
+.l2:	dec	<_cl
+	bne	.l1
+	dec	<_ch
+	bpl	.l1
+	; -- ok
+.ok:	jsr	lib2_bm_disable
+	stz	_bm_error
 	__ldw	<_ax
-	  clc
-	  rts
-	  ; -- error, bad file checksum
-.x1:	  lda	#2
-	  sta	_bm_error
-	  jsr	lib2_bm_disable
-	  clx
-	  cla
-	  sec
-.x2:	  rts
+	clc
+	rts
+	; -- error, bad file checksum
+.x1:	lda	#2
+	sta	_bm_error
+	jsr	lib2_bm_disable
+	clx
+	cla
+	sec
+.x2:	rts
 
 
 ; bm_write(char *buf [di], char *name [bx], int offset [bp], int nb)
@@ -561,40 +561,40 @@ lib2_bm_read.4:
 
 lib2_bm_write.4:
 	__stw	<_ax
-	  ; -- open file
-	  jsr	lib2_bm_open
-	  bcs	.x1
-	  ; -- setup ptr
-	  jsr	lib2_bm_setup_ptr
-	  bcs	.ok
-	  ; -- write data
-	  cly
-.l1:	  lda	[_di],Y
-	  sta	[_dx],Y
-	  iny
-	  bne	.l2
-	  inc	<_dx+1
-	  inc	<_di+1
-.l2:	  dec	<_cl
-	  bne	.l1
-	  dec	<_ch
-	  bpl	.l1
-	  ; -- update checksum
-	  jsr	lib2_bm_checksum
-	  ldy	#2
-	  cla
-	  sub	<_dl
-	  sta	[_si],Y
-	  iny
-	  cla
-	  sbc	<_dh
-	  sta	[_si],Y
-	  ; -- ok
-.ok:	  jsr	lib2_bm_disable
-	  stz	_bm_error
+	; -- open file
+	jsr	lib2_bm_open
+	bcs	.x1
+	; -- setup ptr
+	jsr	lib2_bm_setup_ptr
+	bcs	.ok
+	; -- write data
+	cly
+.l1:	lda	[_di],Y
+	sta	[_dx],Y
+	iny
+	bne	.l2
+	inc	<_dx+1
+	inc	<_di+1
+.l2:	dec	<_cl
+	bne	.l1
+	dec	<_ch
+	bpl	.l1
+	; -- update checksum
+	jsr	lib2_bm_checksum
+	ldy	#2
+	cla
+	sub	<_dl
+	sta	[_si],Y
+	iny
+	cla
+	sbc	<_dh
+	sta	[_si],Y
+	; -- ok
+.ok:	jsr	lib2_bm_disable
+	stz	_bm_error
 	__ldw	<_ax
-	  clc
-.x1:	  rts
+	clc
+.x1:	rts
 
 
 ; bm_create(char *name [bx], int size)
@@ -603,87 +603,87 @@ lib2_bm_write.4:
 
 lib2_bm_create.2:
 	__stw	<_ax
-	  jsr	lib2_bm_enable
-	  bcc	.go
-	  bra	.x2
-	  ; -- error, not enough ram
-.x1:	  lda	#5
-	  ; -- error, bram not formated
-.x2:	  sta	_bm_error
-	  jsr	lib2_bm_disable
-	  ldx	_bm_error
-	  cla
-	  sec
-	  rts
-	  ; -- check free space
-.go:	  addw	#$12,$8006,<_dx
-	  addw	<_ax,<_dx
-	  cmpw	<_dx,$8004
-	  blo	.x1
-	  ; -- create file
-	  stw	$8006,<_si
-	  ldy	#1
-	  lda	<_al
-	  add	#$10
-	  sta	[_si]
-	  lda	<_ah
-	  adc	#$00
-	  sta	[_si],Y
-	  ; --
-	  lda	$8006
-	  add	[_si]
-	  sta	<_dl
-	  sta	$8006
-	  lda	$8007
-	  adc	[_si],Y
-	  sta	<_dh
-	  sta	$8007
-	  cla
-	  sta	[_dx]
-	  sta	[_dx],Y
-	  ; -- copy name
-	  clx
-	  ldy	#4
-.l1:	  sxy
-	  lda	[_bx],Y
-	  sxy
-	  sta	[_si],Y
-	  iny
-	  inx
-	  cpx	#12
-	  bne	.l1
-	  ; -- clear file
-	  lda	<_al
-	  ora	<_ah
-	  beq	.sum
-	  stw	<_si,<_bx
-	  ldy	#16
-	  cla
-.l2:	  sta	[_bx],Y
-	  iny
-	  bne	.l3
-	  inc	<_bh
-.l3:	  dec	<_al
-	  bne	.l2
-	  dec	<_ah
-	  bpl	.l2
-	  ; -- update checksum
-.sum:	  jsr	lib2_bm_checksum
-	  ldy	#2
-	  cla
-	  sub	<_dl
-	  sta	[_si],Y
-	  iny
-	  cla
-	  sbc	<_dh
-	  sta	[_si],Y
-	  ; -- ok
-.ok:	  jsr	lib2_bm_disable
-	  stz	_bm_error
-	  clx
-	  cla
-	  clc
-	  rts
+	jsr	lib2_bm_enable
+	bcc	.go
+	bra	.x2
+	; -- error, not enough ram
+.x1:	lda	#5
+	; -- error, bram not formatted
+.x2:	sta	_bm_error
+	jsr	lib2_bm_disable
+	ldx	_bm_error
+	cla
+	sec
+	rts
+	; -- check free space
+.go:	addw	#$12,$8006,<_dx
+	addw	<_ax,<_dx
+	cmpw	<_dx,$8004
+	blo	.x1
+	; -- create file
+	stw	$8006,<_si
+	ldy	#1
+	lda	<_al
+	add	#$10
+	sta	[_si]
+	lda	<_ah
+	adc	#$00
+	sta	[_si],Y
+	; --
+	lda	$8006
+	add	[_si]
+	sta	<_dl
+	sta	$8006
+	lda	$8007
+	adc	[_si],Y
+	sta	<_dh
+	sta	$8007
+	cla
+	sta	[_dx]
+	sta	[_dx],Y
+	; -- copy name
+	clx
+	ldy	#4
+.l1:	sxy
+	lda	[_bx],Y
+	sxy
+	sta	[_si],Y
+	iny
+	inx
+	cpx	#12
+	bne	.l1
+	; -- clear file
+	lda	<_al
+	ora	<_ah
+	beq	.sum
+	stw	<_si,<_bx
+	ldy	#16
+	cla
+.l2:	sta	[_bx],Y
+	iny
+	bne	.l3
+	inc	<_bh
+.l3:	dec	<_al
+	bne	.l2
+	dec	<_ah
+	bpl	.l2
+	; -- update checksum
+.sum:	jsr	lib2_bm_checksum
+	ldy	#2
+	cla
+	sub	<_dl
+	sta	[_si],Y
+	iny
+	cla
+	sbc	<_dh
+	sta	[_si],Y
+	; -- ok
+.ok:	jsr	lib2_bm_disable
+	stz	_bm_error
+	clx
+	cla
+	clc
+	rts
 
 
 ; bm_open(char *name [bx])
@@ -691,60 +691,60 @@ lib2_bm_create.2:
 ; Internal function to obtain access to a named file
 
 lib2_bm_open:
-	  jsr	lib2_bm_enable
-	  bcs	.x2
-	  ; -- get dir entry
-	  stw	#$8010,<_si
-.l1:	  lda	[_si]
-	  sta	<_cl
-	  ldy	#1
-	  lda	[_si],Y
-	  sta	<_ch
-	  ora	<_cl
-	  beq	.x1
-	  addw	<_cx,<_si,<_dx
-	  cmpw	<_dx,$8004
-	  blo	.x3
-	  cmpw	#16,<_cx
-	  blo	.x3
-	  ; -- compare names
-	  cly
-	  ldx	#4
-.l2:	  lda	[_bx],Y
-	  sxy
-	  cmp	[_si],Y
-	  bne	.next
-	  sxy
-	  inx
-	  iny
-	  cpy	#12
-	  blo	.l2
-	  ; -- check file size
-	  lda	<_ch
-	  bne	.ok
-	  lda	<_cl
-	  cmp	#16
-	  beq	.x4
-	  ; -- ok
-.ok:	  stz	_bm_error
-	  clc
-	  rts
-	  ; -- next entry
-.next:	  addw	<_cx,<_si
-	  bra	.l1
-	  ; -- error, file not found
-.x1:	  lda	#1
-	  ; -- error, bram not formated
-.x2:	  sta	_bm_error
-	  jsr	lib2_bm_disable
-	  sec
-	  rts
-	  ; -- error, directory corrupted
-.x3:	  lda	#3
-	  bra	.x2
-	  ; -- error, empty file
-.x4:	  lda	#4
-	  bra	.x2
+	jsr	lib2_bm_enable
+	bcs	.x2
+	; -- get dir entry
+	stw	#$8010,<_si
+.l1:	lda	[_si]
+	sta	<_cl
+	ldy	#1
+	lda	[_si],Y
+	sta	<_ch
+	ora	<_cl
+	beq	.x1
+	addw	<_cx,<_si,<_dx
+	cmpw	<_dx,$8004
+	blo	.x3
+	cmpw	#16,<_cx
+	blo	.x3
+	; -- compare names
+	cly
+	ldx	#4
+.l2:	lda	[_bx],Y
+	sxy
+	cmp	[_si],Y
+	bne	.next
+	sxy
+	inx
+	iny
+	cpy	#12
+	blo	.l2
+	; -- check file size
+	lda	<_ch
+	bne	.ok
+	lda	<_cl
+	cmp	#16
+	beq	.x4
+	; -- ok
+.ok:	stz	_bm_error
+	clc
+	rts
+	; -- next entry
+.next:	addw	<_cx,<_si
+	bra	.l1
+	; -- error, file not found
+.x1:	lda	#1
+	; -- error, bram not formatted
+.x2:	sta	_bm_error
+	jsr	lib2_bm_disable
+	sec
+	rts
+	; -- error, directory corrupted
+.x3:	lda	#3
+	bra	.x2
+	; -- error, empty file
+.x4:	lda	#4
+	bra	.x2
 
 
 ; bm_enable()
@@ -752,22 +752,22 @@ lib2_bm_open:
 ; Internal function to enable the BRAM area and do a quick check
 
 lib2_bm_enable:
-	  jsr	lib2_bm_unlock
-	  ; -- check if formated
-	  ldx	#3
-.l1:	  lda	$8000,X
-	  cmp	_bm_id,X
-	  bne	.x1
-	  dex
-	  bpl	.l1
-	  ; -- ok
-	  cla
-	  clc
-	  rts
-	  ; -- error, not formated!
-.x1:	  lda	#$FF
-	  sec
-	  rts
+	jsr	lib2_bm_unlock
+	; -- check if formatted
+	ldx	#3
+.l1:	lda	$8000,X
+	cmp	_bm_id,X
+	bne	.x1
+	dex
+	bpl	.l1
+	; -- ok
+	cla
+	clc
+	rts
+	; -- error, not formatted!
+.x1:	lda	#$FF
+	sec
+	rts
 
 
 ; bm_unlock()
@@ -775,15 +775,15 @@ lib2_bm_enable:
 ; This internal function handles only the map/unlock of the BRAM area
 
 lib2_bm_unlock:
-	  sei
-	  tma	#4
-	  sta	_bm_mpr4
-	  lda	#$F7
-	  tam	#4
-	  csl
-	  lda	#$80
-	  sta	bram_unlock
-	  rts
+	sei
+	tma	#4
+	sta	_bm_mpr4
+	lda	#$F7
+	tam	#4
+	csl
+	lda	#$80
+	sta	bram_unlock
+	rts
 
 	
 ; bm_disable()
@@ -791,12 +791,12 @@ lib2_bm_unlock:
 ; This internal function handles the fixup of BRAM segment/locking
 
 lib2_bm_disable:
-	  lda	_bm_mpr4
-	  tam	#4
-	  lda	bram_lock
-	  csh
-	  cli
-	  rts
+	lda	_bm_mpr4
+	tam	#4
+	lda	bram_lock
+	csh
+	cli
+	rts
 
 
 ; bm_checksum(char *fcb [si])
@@ -805,72 +805,72 @@ lib2_bm_disable:
 ;
 
 lib2_bm_checksum:
-	  stwz	<_dx
-	  ; -- get file size
-	  lda	[_si]
-	  sub	#4
-	  sta	<_cl
-	  ldy	#1
-	  lda	[_si],Y
-	  sbc	#0
-	  sta	<_ch
-	  stw	<_si,<_bx
-	  ; -- calc checksum
-	  ldy	#4
-.l1:	  lda	[_bx],Y
-	  add	<_dl
-	  sta	<_dl
-	  bcc	.l2
-	  inc	<_dh
-.l2:	  iny
-	  bne	.l3
-	  inc	<_bh
-.l3:	  dec	<_cl
-	  bne	.l1
-	  dec	<_ch
-	  bpl	.l1
-	  rts
+	stwz	<_dx
+	; -- get file size
+	lda	[_si]
+	sub	#4
+	sta	<_cl
+	ldy	#1
+	lda	[_si],Y
+	sbc	#0
+	sta	<_ch
+	stw	<_si,<_bx
+	; -- calc checksum
+	ldy	#4
+.l1:	lda	[_bx],Y
+	add	<_dl
+	sta	<_dl
+	bcc	.l2
+	inc	<_dh
+.l2:	iny
+	bne	.l3
+	inc	<_bh
+.l3:	dec	<_cl
+	bne	.l1
+	dec	<_ch
+	bpl	.l1
+	rts
 
 
 ; bm_setup_ptr(char *fcb [si], char *buf [di], int offset [bp], int nb [ax])
 ; ---
 
 lib2_bm_setup_ptr:
-	  ; -- check length
-	  tstw	<_ax
-	  beq	.x1
-	  ; -- check ptr
-	  tstw	<_di
-	  beq	.x1
-	  ; -- check offset
-	  addw	#16,<_bp,<_bx
-	  ldy	#1
-	  lda	<_bh
-	  cmp	[_si],Y
-	  bne	.l1
-	  lda	<_bl
-	  cmp	[_si]
-.l1:	  blo	.l2
-	  ; -- eof
-.x1:	  stwz	<_ax
-	  sec
-	  rts
-	  ; -- set base ptr
-.l2:	  addw	<_bx,<_si,<_dx
-	  ; -- check length
-	  addw	<_ax,<_bx
-	  lda	[_si]
-	  sub	<_bl
-	  sta	<_bl
-	  lda	[_si],Y
-	  sbc	<_bh
-	  sta	<_bh
-	  bpl	.ok
-	  ; -- adjust size
-	  addw	<_bx,<_ax
-.ok:	  stw	<_ax,<_cx
-	  clc
-	  rts
+	; -- check length
+	tstw	<_ax
+	beq	.x1
+	; -- check ptr
+	tstw	<_di
+	beq	.x1
+	; -- check offset
+	addw	#16,<_bp,<_bx
+	ldy	#1
+	lda	<_bh
+	cmp	[_si],Y
+	bne	.l1
+	lda	<_bl
+	cmp	[_si]
+.l1:	blo	.l2
+	; -- eof
+.x1:	stwz	<_ax
+	sec
+	rts
+	; -- set base ptr
+.l2:	addw	<_bx,<_si,<_dx
+	; -- check length
+	addw	<_ax,<_bx
+	lda	[_si]
+	sub	<_bl
+	sta	<_bl
+	lda	[_si],Y
+	sbc	<_bh
+	sta	<_bh
+	bpl	.ok
+	; -- adjust size
+	addw	<_bx,<_ax
+.ok:	stw	<_ax,<_cx
+	clc
+	rts
 
 ;
 ; restore the context of the library routines
