6261b5c880b035eb1be9fdd9cd7b6cda982e47b8
Ulrich Hecht <ulrich.hecht@gmail.com>
2014-11-24 18:37:54 +0100

    huc: struct.*: uncrustify
    Signed-off-by: Ulrich Hecht <ulrich.hecht@gmail.com>

--- src/huc/struct.c
+++ src/huc/struct.c
@@ -15,17 +15,18 @@
  * @param sname
  * @return index
  */
-int find_tag(char *sname) {
-    int index;
-    
-    index = 0;
-    while (index < tag_table_index) {
-        if (astreq (sname, tag_table[index].name, NAMEMAX)) {
-            return index;
-        }
-        ++index;
-    }
-    return -1;
+int find_tag (char *sname)
+{
+	int index;
+
+	index = 0;
+	while (index < tag_table_index) {
+		if (astreq(sname, tag_table[index].name, NAMEMAX))
+			return (index);
+
+		++index;
+	}
+	return (-1);
 }
 
 /**
@@ -34,17 +35,19 @@ int find_tag(char *sname) {
  * @param sname
  * @return pointer to member symbol if it is, else 0
  */
-SYMBOL *find_member(TAG_SYMBOL *tag, char *sname) {
-    int member_idx;
+SYMBOL *find_member (TAG_SYMBOL *tag, char *sname)
+{
+	int member_idx;
 
-    member_idx = tag->member_idx;
+	member_idx = tag->member_idx;
 
-    while (member_idx < tag->member_idx + tag->number_of_members) {
-        if (strcmp(member_table[member_idx].name, sname) == 0)
-            return &member_table[member_idx];
-        ++member_idx;
-    }
-    return 0;
+	while (member_idx < tag->member_idx + tag->number_of_members) {
+		if (strcmp(member_table[member_idx].name, sname) == 0)
+			return (&member_table[member_idx]);
+
+		++member_idx;
+	}
+	return (0);
 }
 
 /**
@@ -54,55 +57,59 @@ SYMBOL *find_member(TAG_SYMBOL *tag, char *sname) {
  * @param typ
  * @param offset
  * @param storage
- * @return 
+ * @return
  */
-void add_member(char *sname, char identity, char type, int offset, int storage_class, int otag, int ptr_order) {
-    char *buffer_ptr;
-    SYMBOL *symbol;
-    if (member_table_index >= NUMMEMB) {
-        error("symbol table overflow");
-        return;
-    }
-    symbol = &member_table[member_table_index];
-    buffer_ptr = symbol->name;
-    while (an(*buffer_ptr++ = *sname++));
-    symbol->ident = identity;
-    symbol->type = type;
-    symbol->storage = storage_class;
-    symbol->offset = offset;
-    symbol->ptr_order = ptr_order;
-    if (type == CSTRUCT)
-	    symbol->tagidx = otag;
+void add_member (char *sname, char identity, char type, int offset, int storage_class, int otag, int ptr_order)
+{
+	char *buffer_ptr;
+	SYMBOL *symbol;
 
-    member_table_index++;
+	if (member_table_index >= NUMMEMB) {
+		error("symbol table overflow");
+		return;
+	}
+	symbol = &member_table[member_table_index];
+	buffer_ptr = symbol->name;
+	while (an(*buffer_ptr++ = *sname++)) ;
+	symbol->ident = identity;
+	symbol->type = type;
+	symbol->storage = storage_class;
+	symbol->offset = offset;
+	symbol->ptr_order = ptr_order;
+	if (type == CSTRUCT)
+		symbol->tagidx = otag;
+
+	member_table_index++;
 }
 
-int define_struct(char *sname, int storage, int is_struct) {
-    TAG_SYMBOL *symbol;
-    char *buffer_ptr;
-    int tti;
+int define_struct (char *sname, int storage, int is_struct)
+{
+	TAG_SYMBOL *symbol;
+	char *buffer_ptr;
+	int tti;
 
-    //tag_table_index++;
-    if (tag_table_index >= NUMTAG) {
-        error("struct table overflow");
-        return 0;
-    }
-    tti = tag_table_index++;
-    symbol = &tag_table[tti];
-    buffer_ptr = symbol->name;
-    while (an(*buffer_ptr++ = *sname++));
-    symbol->size = 0;
-    symbol->member_idx = member_table_index;
+	// tag_table_index++;
+	if (tag_table_index >= NUMTAG) {
+		error("struct table overflow");
+		return (0);
+	}
+	tti = tag_table_index++;
+	symbol = &tag_table[tti];
+	buffer_ptr = symbol->name;
+	while (an(*buffer_ptr++ = *sname++)) ;
+	symbol->size = 0;
+	symbol->member_idx = member_table_index;
 
-    needbrack("{");
-    while (!match("}")) {
-        if (!dodcls(storage, &tag_table[tti], is_struct))
-            break;
-        /* We need to keep number_of_members up-to-date to make sure that
-           find_member() works so we can catch duplicate member names. */
+	needbrack("{");
+	while (!match("}")) {
+		if (!dodcls(storage, &tag_table[tti], is_struct))
+			break;
+		/* We need to keep number_of_members up-to-date to make sure that
+		   find_member() works so we can catch duplicate member names. */
+		symbol->number_of_members = member_table_index - symbol->member_idx;
+	}
+	;
+	/* XXX: still necessary? */
 	symbol->number_of_members = member_table_index - symbol->member_idx;
-    };
-    /* XXX: still necessary? */
-    symbol->number_of_members = member_table_index - symbol->member_idx;
-    return tti;
+	return (tti);
 }
--- src/huc/struct.h
+++ src/huc/struct.h
@@ -1,4 +1,3 @@
-int find_tag(char *sname);
-int define_struct(char *sname, int storage, int is_struct);
-void add_member(char *sname, char identity, char type, int offset, int storage_class, int otag, int ptr_order);
-
+int find_tag (char *sname);
+int define_struct (char *sname, int storage, int is_struct);
+void add_member (char *sname, char identity, char type, int offset, int storage_class, int otag, int ptr_order);
