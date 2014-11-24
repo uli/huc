a766d8749921e452ce7c50f3b5d94978c7a75a5f
Ulrich Hecht <ulrich.hecht@gmail.com>
2014-11-24 16:22:23 +0100

    huc: data.*: uncrustify, convert from CRLF to LF
    Signed-off-by: Ulrich Hecht <ulrich.hecht@gmail.com>

--- src/huc/data.c
+++ src/huc/data.c
@@ -9,85 +9,85 @@
 /* constant arrays storage */
 
 struct const_array *const_ptr;
-struct const_array  const_var[MAX_CONST];
-long  const_val[MAX_CONST_VALUE];
+struct const_array const_var[MAX_CONST];
+long const_val[MAX_CONST_VALUE];
 char const_data[MAX_CONST_DATA];
-long  const_val_start;
-long  const_val_idx;
-long  const_data_start;
-long  const_data_idx;
-long  const_size;
-long  const_nb;
+long const_val_start;
+long const_val_idx;
+long const_data_start;
+long const_data_idx;
+long const_size;
+long const_nb;
 
 /* storage words */
 
 SYMBOL symtab[SYMTBSZ];
 SYMBOL *glbptr, *rglbptr, *locptr;
-long  ws[WSTABSZ];
-long  *wsptr;
-long   swstcase[SWSTSZ];
-long   swstlab[SWSTSZ];
-long   swstp;
-char  litq[LITABSZ];
-char  litq2[LITABSZ];
-long   litptr;
-struct macro  macq[MACQSIZE];
-long   macptr;
-char  line[LINESIZE];
-char  mline[LINESIZE];
-long   lptr, mptr;
+long ws[WSTABSZ];
+long *wsptr;
+long swstcase[SWSTSZ];
+long swstlab[SWSTSZ];
+long swstp;
+char litq[LITABSZ];
+char litq2[LITABSZ];
+long litptr;
+struct macro macq[MACQSIZE];
+long macptr;
+char line[LINESIZE];
+char mline[LINESIZE];
+long lptr, mptr;
 
-TAG_SYMBOL  tag_table[NUMTAG]; // start of structure tag table
-int         tag_table_index; // ptr to next entry
+TAG_SYMBOL tag_table[NUMTAG];	// start of structure tag table
+int tag_table_index;		// ptr to next entry
 
-SYMBOL	member_table[NUMMEMB];	// structure member table
-int	member_table_index;	// ptr to next member
+SYMBOL member_table[NUMMEMB];	// structure member table
+int member_table_index;		// ptr to next member
 
-char  asmdefs[LITABSZ];
+char asmdefs[LITABSZ];
 
 /* miscellaneous storage */
 
-long	nxtlab,
-	litlab,
-	stkp,
-	zpstkp,
-	argstk,
-	ncmp,
-	errcnt,
-	glbflag,
-	indflg,
-	ctext,
-	cmode,
-	lastst,
-	overlayflag,
-	optimize,
-	globals_h_in_process;
+long nxtlab,
+     litlab,
+     stkp,
+     zpstkp,
+     argstk,
+     ncmp,
+     errcnt,
+     glbflag,
+     indflg,
+     ctext,
+     cmode,
+     lastst,
+     overlayflag,
+     optimize,
+     globals_h_in_process;
 
 long top_level_stkp;
 
-FILE	*input, *input2, *output;
-FILE	*inclstk[INCLSIZ];
+FILE *input, *input2, *output;
+FILE *inclstk[INCLSIZ];
 
-char    inclstk_name[INCLSIZ][FILENAMESIZE];
-char    fname_copy[FILENAMESIZE];
-char	user_outfile[FILENAMESIZE];
-long     inclstk_line[INCLSIZ];
-long     line_number;
+char inclstk_name[INCLSIZ][FILENAMESIZE];
+char fname_copy[FILENAMESIZE];
+char user_outfile[FILENAMESIZE];
+long inclstk_line[INCLSIZ];
+long line_number;
 
-long		inclsp;
-char	fname[FILENAMESIZE];
+long inclsp;
+char fname[FILENAMESIZE];
 
-char	quote[2];
-char	*cptr;
-long	*iptr;
-long	fexitlab;
-long	iflevel, skiplevel;
-long	errfile;
-long	sflag;
-long	cdflag;
-long	verboseflag;
-long	startup_incl;
-long	errs;
+char quote[2];
+char *cptr;
+long *iptr;
+long fexitlab;
+long iflevel, skiplevel;
+long errfile;
+long sflag;
+long cdflag;
+long verboseflag;
+long startup_incl;
+long errs;
 
 int norecurse = 0;
 long locals_ptr;
@@ -116,5 +116,5 @@ int leaf_cnt = 0;
 int leaf_size = 0;
 
 INITIALS initials_table[NUMGLBS];
-char initials_data_table[INITIALS_SIZE];      // 5kB space for initialisation data
+char initials_data_table[INITIALS_SIZE];	// 5kB space for initialisation data
 int initials_idx = 0, initials_data_idx = 0;
--- src/huc/data.h
+++ src/huc/data.h
@@ -7,83 +7,83 @@
 /* constant arrays storage */
 
 extern struct const_array *const_ptr;
-extern struct const_array  const_var[MAX_CONST];
-extern long  const_val[MAX_CONST_VALUE];
+extern struct const_array const_var[MAX_CONST];
+extern long const_val[MAX_CONST_VALUE];
 extern char const_data[MAX_CONST_DATA];
-extern long  const_val_start;
-extern long  const_val_idx;
-extern long  const_data_start;
-extern long  const_data_idx;
-extern long  const_size;
-extern long  const_nb;
+extern long const_val_start;
+extern long const_val_idx;
+extern long const_data_start;
+extern long const_data_idx;
+extern long const_size;
+extern long const_nb;
 
 /* storage words */
 
-extern	SYMBOL symtab[];
-extern	SYMBOL *glbptr, *rglbptr, *locptr;
-extern	long   ws[];
-extern	long  *wsptr;
-extern	long   swstcase[];
-extern	long   swstlab[];
-extern	long   swstp;
-extern	char  litq[];
-extern	char  litq2[];
-extern	long   litptr;
-extern	struct macro  macq[];
-extern	long   macptr;
-extern	char  line[];
-extern	char  mline[];
-extern	long   lptr, mptr;
+extern SYMBOL symtab[];
+extern SYMBOL *glbptr, *rglbptr, *locptr;
+extern long ws[];
+extern long *wsptr;
+extern long swstcase[];
+extern long swstlab[];
+extern long swstp;
+extern char litq[];
+extern char litq2[];
+extern long litptr;
+extern struct macro macq[];
+extern long macptr;
+extern char line[];
+extern char mline[];
+extern long lptr, mptr;
 
-extern TAG_SYMBOL  tag_table[NUMTAG]; // start of structure tag table
-extern int	   tag_table_index;   // ptr to next entry
+extern TAG_SYMBOL tag_table[NUMTAG];	// start of structure tag table
+extern int tag_table_index;		// ptr to next entry
 
-extern SYMBOL	member_table[NUMMEMB];	// structure member table
-extern int	member_table_index;	// ptr to next member<
+extern SYMBOL member_table[NUMMEMB];	// structure member table
+extern int member_table_index;		// ptr to next member<
 
-extern  char  asmdefs[];
+extern char asmdefs[];
 
 /* miscellaneous storage */
 
-extern	long	nxtlab,
-		litlab,
-		stkp,
-		zpstkp,
-		argstk,
-		ncmp,
-		errcnt,
-		glbflag,
-		indflg,
-		ctext,
-		cmode,
-		lastst,
-    overlayflag,
-    optimize,
-		globals_h_in_process;
+extern long nxtlab,
+	    litlab,
+	    stkp,
+	    zpstkp,
+	    argstk,
+	    ncmp,
+	    errcnt,
+	    glbflag,
+	    indflg,
+	    ctext,
+	    cmode,
+	    lastst,
+	    overlayflag,
+	    optimize,
+	    globals_h_in_process;
 
-extern	FILE	*input, *input2, *output;
-extern	FILE	*inclstk[];
+extern FILE *input, *input2, *output;
+extern FILE *inclstk[];
 
-extern char    inclstk_name[INCLSIZ][FILENAMESIZE];
-extern long     inclstk_line[];
-extern char    fname_copy[FILENAMESIZE];
-extern char	user_outfile[FILENAMESIZE];
-extern long     line_number;
+extern char inclstk_name[INCLSIZ][FILENAMESIZE];
+extern long inclstk_line[];
+extern char fname_copy[FILENAMESIZE];
+extern char user_outfile[FILENAMESIZE];
+extern long line_number;
 
-extern	long		inclsp;
-extern	char	fname[];
+extern long inclsp;
+extern char fname[];
 
-extern	char	quote[];
-extern	SYMBOL	*cptr;
-extern	long	*iptr;
-extern	long	fexitlab;
-extern	long	iflevel, skiplevel;
-extern	long	errfile;
-extern	long	sflag;
-extern	long	cdflag;
-extern	long	verboseflag;
-extern	long	startup_incl;
-extern	long	errs;
+extern char quote[];
+extern SYMBOL *cptr;
+extern long *iptr;
+extern long fexitlab;
+extern long iflevel, skiplevel;
+extern long errfile;
+extern long sflag;
+extern long cdflag;
+extern long verboseflag;
+extern long startup_incl;
+extern long errs;
 
 extern long top_level_stkp;
 extern int norecurse;
@@ -114,5 +114,5 @@ extern int leaf_cnt;
 extern int leaf_size;
 
 extern INITIALS initials_table[NUMGLBS];
-extern char initials_data_table[INITIALS_SIZE];      // 5kB space for initialisation data
+extern char initials_data_table[INITIALS_SIZE];		// 5kB space for initialisation data
 extern int initials_idx, initials_data_idx;
