442a552e467efb98fd9f9d87596fffc8acbc5bad
Ulrich Hecht <ulrich.hecht@gmail.com>
2014-11-24 18:02:58 +0100

    huc: initials.*: uncrustify
    (Except the commented-out code, uncrustify fails at that...)
    
    Signed-off-by: Ulrich Hecht <ulrich.hecht@gmail.com>

--- src/huc/initials.c
+++ src/huc/initials.c
@@ -26,9 +26,10 @@ void create_initials() {
  * @param symbol_name
  * @param type
  */
-void add_symbol_initials(char *symbol_name, char type) {
-    strcpy(initials_table[initials_idx].name, symbol_name);
-    initials_table[initials_idx].type = type;
+void add_symbol_initials (char *symbol_name, char type)
+{
+	strcpy(initials_table[initials_idx].name, symbol_name);
+	initials_table[initials_idx].type = type;
 }
 
 /**
@@ -36,22 +37,25 @@ void add_symbol_initials(char *symbol_name, char type) {
  * @param symbol_name
  * @return
  */
-int find_symbol_initials(char *symbol_name) {
-    int result = 0;
-    initials_data_idx = 0;
-    for (initials_idx=0; initials_table[initials_idx].type != 0; initials_idx++) {
-        if (initials_idx >= NUMGLBS) {
-            error("initials table overrun");
-        }
-        if (astreq (symbol_name, initials_table[initials_idx].name, NAMEMAX) != 0) {
-            result = 1;
-            break;
-        } else { // move to next symbol
-            // count position in data array
-            initials_data_idx += initials_table[initials_idx].data_len;
-        }
-    }
-    return result;
+int find_symbol_initials (char *symbol_name)
+{
+	int result = 0;
+
+	initials_data_idx = 0;
+	for (initials_idx = 0; initials_table[initials_idx].type != 0; initials_idx++) {
+		if (initials_idx >= NUMGLBS)
+			error("initials table overrun");
+		if (astreq(symbol_name, initials_table[initials_idx].name, NAMEMAX) != 0) {
+			result = 1;
+			break;
+		}
+		else {
+			// move to next symbol
+			// count position in data array
+			initials_data_idx += initials_table[initials_idx].data_len;
+		}
+	}
+	return (result);
 }
 
 /**
@@ -61,29 +65,32 @@ int find_symbol_initials(char *symbol_name) {
  * @param value
  * @param tag
  */
-void add_data_initials(char *symbol_name, int type, int value, TAG_SYMBOL *tag) {
-    int position;
-    if (find_symbol_initials(symbol_name) == 0) {
-        add_symbol_initials(symbol_name, tag == 0 ? type : CSTRUCT);
-    }
-    if (tag != 0) {
-        // find number of members, dim is total number of values added
-        int index = initials_table[initials_idx].dim % tag->number_of_members;
-        int member_type = member_table[tag->member_idx + index].type;
-        // add it recursively
-        add_data_initials(symbol_name, member_type, value, 0);
-    } else {
-        position = initials_table[initials_idx].data_len;
-        if (type == CCHAR || type == CUCHAR) {
-            initials_data_table[initials_data_idx + position] = 0xff & value;
-            initials_table[initials_idx].data_len += 1;
-        } else {
-            initials_data_table[initials_data_idx + position] = (0xff00 & value) >> 8;
-            initials_data_table[initials_data_idx + position + 1] = 0xff & value;
-            initials_table[initials_idx].data_len += INTSIZE;
-        }
-        initials_table[initials_idx].dim += 1;
-    }
+void add_data_initials (char *symbol_name, int type, int value, TAG_SYMBOL *tag)
+{
+	int position;
+
+	if (find_symbol_initials(symbol_name) == 0)
+		add_symbol_initials(symbol_name, tag == 0 ? type : CSTRUCT);
+	if (tag != 0) {
+		// find number of members, dim is total number of values added
+		int index = initials_table[initials_idx].dim % tag->number_of_members;
+		int member_type = member_table[tag->member_idx + index].type;
+		// add it recursively
+		add_data_initials(symbol_name, member_type, value, 0);
+	}
+	else {
+		position = initials_table[initials_idx].data_len;
+		if (type == CCHAR || type == CUCHAR) {
+			initials_data_table[initials_data_idx + position] = 0xff & value;
+			initials_table[initials_idx].data_len += 1;
+		}
+		else {
+			initials_data_table[initials_data_idx + position] = (0xff00 & value) >> 8;
+			initials_data_table[initials_data_idx + position + 1] = 0xff & value;
+			initials_table[initials_idx].data_len += INTSIZE;
+		}
+		initials_table[initials_idx].dim += 1;
+	}
 }
 
 /**
@@ -91,12 +98,13 @@ void add_data_initials(char *symbol_name, int type, int value, TAG_SYMBOL *tag) 
  * @param symbol_name
  * @return
  */
-int get_size(char *symbol_name) {
-    int result = 0;
-    if (find_symbol_initials(symbol_name) != 0) {
-        result = initials_table[initials_idx].dim;
-    }
-    return result;
+int get_size (char *symbol_name)
+{
+	int result = 0;
+
+	if (find_symbol_initials(symbol_name) != 0)
+		result = initials_table[initials_idx].dim;
+	return (result);
 }
 
 /**
@@ -106,38 +114,40 @@ int get_size(char *symbol_name) {
  * @param itag index of tag in tag table
  * @return
  */
-int get_item_at(char *symbol_name, int position, TAG_SYMBOL *tag) {
-    int result = 0, i, type;
-    if (find_symbol_initials(symbol_name) != 0) {
-        if ((initials_table[initials_idx].type & ~CUNSIGNED) == CCHAR) {
-            result = initials_data_table[initials_data_idx + position];
-        } else if ((initials_table[initials_idx].type & ~CUNSIGNED) == CINT) {
-            position *= INTSIZE;
-            result = (initials_data_table[initials_data_idx + position] << 8) +
-                    (unsigned char)initials_data_table[initials_data_idx + position+1];
-        } else if (initials_table[initials_idx].type == CSTRUCT) {
-            // find number of members
-            int number_of_members = tag->number_of_members;
-            // point behind the last full struct
-            int index = (position / number_of_members) * tag->size;
-            // move to required member
-            for (i=0; i < (position % number_of_members); i++) {
-                type = member_table[tag->member_idx + i].type;
-                if (type == CCHAR || type == CUCHAR) {
-                    index += 1;
-                } else {
-                    index += INTSIZE;
-                }
-            }
-            // get value
-            type = member_table[tag->member_idx + i].type;
-            if (type == CCHAR || type == CUCHAR) {
-                result = initials_data_table[initials_data_idx + index];
-            } else {
-                result = (initials_data_table[initials_data_idx + index] << 8) +
-                    (unsigned char)initials_data_table[initials_data_idx + index+1];
-            }
-        }
-    }
-    return result;
+int get_item_at (char *symbol_name, int position, TAG_SYMBOL *tag)
+{
+	int result = 0, i, type;
+
+	if (find_symbol_initials(symbol_name) != 0) {
+		if ((initials_table[initials_idx].type & ~CUNSIGNED) == CCHAR)
+			result = initials_data_table[initials_data_idx + position];
+		else if ((initials_table[initials_idx].type & ~CUNSIGNED) == CINT) {
+			position *= INTSIZE;
+			result = (initials_data_table[initials_data_idx + position] << 8) +
+				 (unsigned char)initials_data_table[initials_data_idx + position + 1];
+		}
+		else if (initials_table[initials_idx].type == CSTRUCT) {
+			// find number of members
+			int number_of_members = tag->number_of_members;
+			// point behind the last full struct
+			int index = (position / number_of_members) * tag->size;
+			// move to required member
+			for (i = 0; i < (position % number_of_members); i++) {
+				type = member_table[tag->member_idx + i].type;
+				if (type == CCHAR || type == CUCHAR)
+					index += 1;
+				else
+					index += INTSIZE;
+			}
+			// get value
+			type = member_table[tag->member_idx + i].type;
+			if (type == CCHAR || type == CUCHAR)
+				result = initials_data_table[initials_data_idx + index];
+			else {
+				result = (initials_data_table[initials_data_idx + index] << 8) +
+					 (unsigned char)initials_data_table[initials_data_idx + index + 1];
+			}
+		}
+	}
+	return (result);
 }
--- src/huc/initials.h
+++ src/huc/initials.h
@@ -1,5 +1,5 @@
 #include "defs.h"
-void add_data_initials(char *symbol_name, int type, int value, TAG_SYMBOL *tag);
-int get_item_at(char *symbol_name, int position, TAG_SYMBOL *tag);
-int get_size(char *symbol_name);
-int find_symbol_initials(char *symbol_name);
+void add_data_initials (char *symbol_name, int type, int value, TAG_SYMBOL *tag);
+int get_item_at (char *symbol_name, int position, TAG_SYMBOL *tag);
+int get_size (char *symbol_name);
+int find_symbol_initials (char *symbol_name);
