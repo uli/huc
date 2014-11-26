8487b445444221388fc937e29319bd45440487db
Ulrich Hecht <ulrich.hecht@gmail.com>
2014-11-26 11:57:30 +0100

    mkit: as: map.c: uncrustify
    Signed-off-by: Ulrich Hecht <ulrich.hecht@gmail.com>

--- src/mkit/as/map.c
+++ src/mkit/as/map.c
@@ -37,16 +37,16 @@ pce_load_map(char *fname, int mode)
 	/* check FMP header */
 	fread(header, 1, 12, fp);
 	fsize = (header[4] << 24) +
-			(header[5] << 16) +
-			(header[6] <<  8) +
-			 header[7] - 4;
+		(header[5] << 16) +
+		(header[6] << 8) +
+		header[7] - 4;
 
 	if (memcmp(header, "FORM", 4) || memcmp(&header[8], "FMAP", 4)) {
 		/* incorrect header */
 		if (mode)
 			fatal_error("Invalid FMP format!");
 		fclose(fp);
-		return(mode);
+		return (mode);
 	}
 
 	/* define label */
@@ -60,9 +60,9 @@ pce_load_map(char *fname, int mode)
 	while (fsize > 0) {
 		fread(header, 1, 8, fp);
 		size = (header[4] << 24) +
-			   (header[5] << 16) +
-			   (header[6] <<  8) +
-			    header[7];
+		       (header[5] << 16) +
+		       (header[6] << 8) +
+		       header[7];
 		fsize -= 8;
 		fsize -= size;
 
@@ -82,7 +82,7 @@ pce_load_map(char *fname, int mode)
 
 					/* convert word -> byte */
 					for (i = 0, j = 0; i < nb; i++, j += 2)
-						buffer[i] = ((buffer[j] + (buffer[j+1] << 8)) >> 5) - 1;
+						buffer[i] = ((buffer[j] + (buffer[j + 1] << 8)) >> 5) - 1;
 
 					/* output buffer */
 					putbuffer(buffer, nb);
@@ -95,14 +95,13 @@ pce_load_map(char *fname, int mode)
 			break;
 		}
 		else {
-			if (fsize > 0)
-			{
+			if (fsize > 0) {
 				/* unsupported chunk */
 				fseek(fp, size, SEEK_CUR);
 			}
 		}
 
-		if(fsize < 0)
+		if (fsize < 0)
 			break;
 	}
 
@@ -132,43 +131,43 @@ pce_load_map(char *fname, int mode)
 
 
 
-//The FMP file format
-//The first 12 bytes are as follows:
-//4bytes ASCII = 'FORM'
-//long int = size of file less header (which is filesize-8)
-//4bytes ASCII = 'FMAP'
-//NOTE:  The chunk size long ints like the one above are stored in
-//'Motorola' format, NOT Intel.  You will have to byteswap to get the
-//correct value, ie:  Bytes 1,2,3,4 need to become 4,3,2,1.
+// The FMP file format
+// The first 12 bytes are as follows:
+// 4bytes ASCII = 'FORM'
+// long int = size of file less header (which is filesize-8)
+// 4bytes ASCII = 'FMAP'
+// NOTE:  The chunk size long ints like the one above are stored in
+// 'Motorola' format, NOT Intel.  You will have to byteswap to get the
+// correct value, ie:  Bytes 1,2,3,4 need to become 4,3,2,1.
 //
-//The chunks in the file follow on one after the other, and consist of
-//an 8byte header, and the information specific to that chunk.  See
-//how the playback source reads in the information.  The chunks can be
-//in any order, and some chunks may not be used in a particular file.
+// The chunks in the file follow on one after the other, and consist of
+// an 8byte header, and the information specific to that chunk.  See
+// how the playback source reads in the information.  The chunks can be
+// in any order, and some chunks may not be used in a particular file.
 //
-//Chunk header:
-//4bytes ASCII = ChunkID (example: 'MPHD')
-//long int = size of chunk data less header
+// Chunk header:
+// 4bytes ASCII = ChunkID (example: 'MPHD')
+// long int = size of chunk data less header
 //
-//These are the chunks as of V1.1:
-//ATHR - Up to 4 ASCII strings of author information, separated by 0 values,
-//       always an even size.
-//MPHD - Map header, see struct in mappy.c
-//EDHD - Editor information, see struct in mappy.c
-//CMAP - Colour palette for 8bit maps, red byte, green byte,
-//       blue byte for however many colours are needed (so usually 256*3 bytes).
-//BKDT - Block data. Contains BLKSTR structures for however many block
-//       structures were made.
-//ANDT - Animation data. Contains ANISTR structures for however many
-//       animation structures were made, and also animation data.
-//BGFX - The raw graphics in whatever format the map is in.
-//       Examples: 8bit: mapwidth*mapheight bytes per block,
-//       in forward format *numblocks 16bit: mapwidth*mapheight*2
-//       bytes per block, each word contains 5 bits red, 6 bits green,
-//       5 bits blue.
-//BODY - An array of short ints containing positive offsets into BKDT,
-//       and negative offsets into ANDT.
-//LYR? - Where ? is an ASCII number form 1 to 7.
-//       These are the same size and format as BODY, and allow object
-//       layers to be used.
+// These are the chunks as of V1.1:
+// ATHR - Up to 4 ASCII strings of author information, separated by 0 values,
+//        always an even size.
+// MPHD - Map header, see struct in mappy.c
+// EDHD - Editor information, see struct in mappy.c
+// CMAP - Colour palette for 8bit maps, red byte, green byte,
+//        blue byte for however many colours are needed (so usually 256*3 bytes).
+// BKDT - Block data. Contains BLKSTR structures for however many block
+//        structures were made.
+// ANDT - Animation data. Contains ANISTR structures for however many
+//        animation structures were made, and also animation data.
+// BGFX - The raw graphics in whatever format the map is in.
+//        Examples: 8bit: mapwidth*mapheight bytes per block,
+//        in forward format *numblocks 16bit: mapwidth*mapheight*2
+//        bytes per block, each word contains 5 bits red, 6 bits green,
+//        5 bits blue.
+// BODY - An array of short ints containing positive offsets into BKDT,
+//        and negative offsets into ANDT.
+// LYR? - Where ? is an ASCII number form 1 to 7.
+//        These are the same size and format as BODY, and allow object
+//        layers to be used.
 
